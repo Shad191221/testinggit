26c20dcb2eb8e2f213fa80356d1bd1589d0cc09d
Rinshad <rinshadis19@gmail.com>
2022-09-12 19:11:53 +0530

    first version

---
+++ Coffeerobo.py
@@ -0,0 +1,6 @@
+name = input("What is your name ?")
+print("Greetings!",name)
+menu = "latte,cappucino,blackcoffee"
+print(name+ " today we are serving"+menu)
+choice = input("what shall i order for you?")
+print ("your "+choice+ " will be ready in 10 mins thank you!")
